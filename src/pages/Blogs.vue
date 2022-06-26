efb95c06b80f897a5f323ebe5b63955a57eb45e1
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-06-26 16:40:03 +0400

    third question added

--- src/pages/Blogs.vue
+++ src/pages/Blogs.vue
@@ -1,18 +1,21 @@
 <template>
-     <div className='mt-8 md:mt-16 px-8 space-y-20'>
+     <div className='mt-8 md:mt-16 px-8 md:px-16 space-y-20'>
     <Question1/>
     <Question2/>
+    <Question3/>
    </div>
 </template>
 
 <script>
 import Question1 from "../components/Question1.vue";
 import Question2 from "../components/Question2.vue";
+import Question3 from "../components/Question2.vue";
     export default {
         name: "BlogsA",
         components: {
             Question1,
             Question2,
+            Question3,
         }
     }
 </script>
